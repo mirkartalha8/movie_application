6ca983a239167145df21736cbcd173b21328a98f
talha <mirkartalha8@gmail.com>
2026-06-02 10:48:07 +0530

    database Change

--- movie-list.js
+++ movie-list.js
@@ -7,7 +7,7 @@ const mongoose = require('mongoose');
 const LocalStrategy = require("passport-local"); 
 const path = require("path"); // Import the path module 
 // const uri = 
-//   "mongodb+srv://Admin:Qwerty12345@cluster0.prmpnbi.mongodb.net/?appName=Cluster0"; //MongoDB Atlas Link 
+//   "mongodb+srv://Admin:Qwerty12345@cluster0.prmpnbi.mongodb.net/?appName=Cluster0/Movie"; //MongoDB Atlas Link 
  
 //Connect to MongoDB using MongoDB Compass localhost 
 const uri = 'mongodb://127.0.0.1:27017/Movie'; // MongoDB connection URI 
